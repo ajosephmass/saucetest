d6c438fada8e9fe2df355c7a1913ac844dcdb486
ajosephmass <Ajith.Joseph2@cognizant.com>
2016-12-23 10:52:22 -0500

    Update FollowLinkTest.java

--- src/test/java/com/yourcompany/Tests/FollowLinkTest.java
+++ src/test/java/com/yourcompany/Tests/FollowLinkTest.java
@@ -19,11 +19,17 @@ public class FollowLinkTest extends TestBase {
      */
     @Test
     public void verifyLinkTest() throws InvalidElementStateException {
-        driver.get("http://www.google.com");
-        GuineaPigPage page = GuineaPigPage.visitPage(driver);
+        //driver.get("http://www.google.com");
+        driver.get("https://www.massmutual.com/myaccount/rl/Login/login?type=Customer");
+        driver.findElement(By.id("user-name")).click();
+        driver.findElement(By.id("user-name")).clear();
+        driver.findElement(By.id("user-name")).sendKeys("abcd");
+        driver.findElement(By.xpath("//div[@id='continue-button']/input")).click();
+        /*GuineaPigPage page = GuineaPigPage.visitPage(driver);
 
         page.followLink();
 
         assertFalse(page.isOnPage());
+        */
     }
 }
