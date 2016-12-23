dab1b364fbf2434260fa261f02f2a6e606017a20
ajosephmass <Ajith.Joseph2@cognizant.com>
2016-12-23 11:15:24 -0500

    Update FollowLinkTest.java

--- src/test/java/com/yourcompany/Tests/FollowLinkTest.java
+++ src/test/java/com/yourcompany/Tests/FollowLinkTest.java
@@ -5,6 +5,21 @@ import org.junit.Test;
 import org.openqa.selenium.InvalidElementStateException;
 
 import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.util.concurrent.TimeUnit;
+import java.util.Date;
+import java.io.File;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.*;
+import static org.openqa.selenium.OutputType.*;
 
 public class FollowLinkTest extends TestBase {
 
@@ -21,10 +36,8 @@ public class FollowLinkTest extends TestBase {
     public void verifyLinkTest() throws InvalidElementStateException {
         //driver.get("http://www.google.com");
         driver.get("https://www.massmutual.com/myaccount/rl/Login/login?type=Customer");
-        WebElement username=driver.findElement(By.name("user-name"));
-        username.click();
-        /*driver.findElement(By.id("user-name")).click();
-        driver.findElement(By.id("user-name")).clear();
+        driver.findElement(By.id("user-name")).click();
+        /*driver.findElement(By.id("user-name")).clear();
         driver.findElement(By.id("user-name")).sendKeys("abcd");
         driver.findElement(By.xpath("//div[@id='continue-button']/input")).click();
         GuineaPigPage page = GuineaPigPage.visitPage(driver);
